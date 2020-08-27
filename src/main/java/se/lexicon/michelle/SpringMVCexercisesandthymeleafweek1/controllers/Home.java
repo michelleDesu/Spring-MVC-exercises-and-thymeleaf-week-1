cfa66d9ee142075148e7c2eff8999a3e6f9e576a
michelle <michelle.johansson85@gmail.com>
2020-08-27 10:28:30 +0200

    * Added three methods in controller: * contact, contact and contactList * Added contact.html * Added contactList.html

--- src/main/java/se/lexicon/michelle/SpringMVCexercisesandthymeleafweek1/controllers/Home.java
+++ src/main/java/se/lexicon/michelle/SpringMVCexercisesandthymeleafweek1/controllers/Home.java
@@ -1,14 +1,39 @@
 package se.lexicon.michelle.SpringMVCexercisesandthymeleafweek1.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Home {
+
+    private List<String> strings = new ArrayList<>();
 
     @GetMapping("/index")
     public String index(){
 
         return "index";
+    }
+
+   @GetMapping("/contact")
+    public String contact(){
+
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String contact( @RequestParam("input") String input){
+
+        strings.add(input);
+        return "contact";
+    }
+
+    @GetMapping("/contactList")
+    public String contactList(Model model){
+        model.addAttribute("strings", strings);
+        return "contactList";
     }
 }

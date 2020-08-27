28bbba9b10069e6f09cb3503aa3be5988e326248
michelle <michelle.johansson85@gmail.com>
2020-08-27 07:27:39 +0200

    Init commit

---
+++ src/main/java/se/lexicon/michelle/SpringMVCexercisesandthymeleafweek1/SpringMvcExercisesAndThymeleafWeek1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.michelle.SpringMVCexercisesandthymeleafweek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExercisesAndThymeleafWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcExercisesAndThymeleafWeek1Application.class, args);
+	}
+
+}

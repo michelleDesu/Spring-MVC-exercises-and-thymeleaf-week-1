b221912454719760fdd6ba9e7d15fb82f790fb17
michelle <michelle.johansson85@gmail.com>
2020-08-28 11:56:58 +0200

    * added feverCheck method that redirects to tempResult.html * added tempResult Controller * added tempResult.html

--- src/main/java/se/lexicon/michelle/SpringMVCexercisesandthymeleafweek1/controllers/Fever.java
+++ src/main/java/se/lexicon/michelle/SpringMVCexercisesandthymeleafweek1/controllers/Fever.java
@@ -1,23 +1,44 @@
 package se.lexicon.michelle.SpringMVCexercisesandthymeleafweek1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-/*
-Create a Fever controller and methods with views to handle the following
-• Ask the user for their body temperature
-• Show the user if they have fever /normal / hyperthermia based on body temperature and treatment if needed
-• Use Model
-• You may not use JavaScript
- */
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Fever {
+
+    private String temperatureResult;
 
     @GetMapping("/feverCheck")
     public String feverCheck(){
 
         return "feverCheck";
+    }
+
+    @PostMapping("/feverCheck")
+    public String feverCheck(@RequestParam("temperature") String temperature){
+
+        String replaceTemp = temperature.replace(",", ".");
+
+        double temp = Double.parseDouble(replaceTemp);
+
+        if(temp < 35){
+            temperatureResult = "You Have Hypothermia";
+        }else if(temp >= 35 && temp < 38){
+            temperatureResult = "Your Temperature Is Normal";
+        }else{
+            temperatureResult = "You Have A Fever";
+        }
+        return "redirect:/tempResult";
+    }
+
+    @GetMapping("/tempResult")
+    public String tempResult(Model model){
+        model.addAttribute("temperatureResult", temperatureResult );
+
+        return "tempResult";
     }
 
 }
